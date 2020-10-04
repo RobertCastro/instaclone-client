2290c046c850cd0de72831225621eef238b87f08
Robert Castro <oneroberth@gmail.com>
2020-10-04 12:45:53 -0500

    Separando zonas auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo";
+import Auth from "./pages/Auth";
 
 function App() {
+  const [auth, setAuth] = useState({ name: "Robert" });
+
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <h1>Estamos en APP</h1>
-        <div>
-          <Button primary>Primary</Button>
-          <Button secondary>Secondary</Button>
-        </div>
-      </div>
+      {!auth ? <Auth /> : <h1>Estas logueado</h1>}
     </ApolloProvider>
   );
 }
