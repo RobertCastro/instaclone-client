9aabb5a8af7856954603918a5a71911ce13fd34f
Robert Castro <oneroberth@gmail.com>
2020-10-13 18:09:07 -0500

    Control de datos con formik

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
 import "./RegisterForm.scss";
 
 export default function RegisterForm({ setShowLogin }) {
 
-    const onSubmit = () => {
-        console.log("enviando")
-    }
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: null,
+        onSubmit: (formValue) => {
+            console.log(formValue)
+        }
+    })
+
     return (
         <>
             <h2 className="register-form-title">Regístrate para ver fotos y videos</h2>
-            <Form className="register-form" onSubmit={onSubmit}>
-                <Form.Input type="text" placeholder="Nombre y apellidos" name="name" />
-                <Form.Input type="text" placeholder="Nombre de usuario" name="name" />
-                <Form.Input type="text" placeholder="Correo electrónico" name="email" />
-                <Form.Input type="password" placeholder="Contraseña" name="password" />
-                <Form.Input type="password" placeholder="Repetir Contraseña" name="repeatPassword" />
+            <Form className="register-form" onSubmit={formik.handleSubmit}>
+                <Form.Input type="text" placeholder="Nombre y apellidos" name="name" onChange={formik.handleChange} />
+                <Form.Input type="text" placeholder="Nombre de usuario" name="username" onChange={formik.handleChange} />
+                <Form.Input type="text" placeholder="Correo electrónico" name="email" onChange={formik.handleChange} />
+                <Form.Input type="password" placeholder="Contraseña" name="password" onChange={formik.handleChange} />
+                <Form.Input type="password" placeholder="Repetir Contraseña" name="repeatPassword" onChange={formik.handleChange} />
                 <Button type="submit" className="btn-submit">Registrarse</Button>
             </Form>
         </>
     )
+}
+
+function initialValues() {
+    return {
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+    }
 }
